6c4d93a88f583a81fb549e3d0318074dcd901c9e
flum1025 <flum.1025@gmail.com>
2016-01-29 16:17:00 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+EPNs-client
+===========
+
+##What is it?
+
+EPNs用のrubyクライアントです。詳細は[EPNs](http://epns.flum.pw)
+
+動作確認はruby 2.0.0p481及びruby 2.1.0ですがruby 1.9.3でも動くと思います。
+
+##How to Use
+Rubyのインストールは省きます。 
+準備としてEPNs-client/に移動しbundle installを実行してください。  
+　  
+まず、[EPNsのトップページ](http://epns.flum.pw/)でユーザー登録をし、api_keyを取得してください。  
+必要なデータをクライアントに指定すれば接続できます。  
+sample/にデモ用のclient.rbと説明が書いてるので参考にしてください。  
+　　  
+
+質問等ありましたらTwitter:[@flum_](https://twitter.com/flum_)までお願いします。  
+
+##License
+
+The MIT License
+
+-------
+(c) @2015 flum_
--- sample/client.rb
+++ sample/client.rb
@@ -1,15 +1,13 @@
 path = File.expand_path('../', __FILE__)
 require File.join(path, '../lib/EPNs.rb')
 
-$host = "http://localhost:3000"
+#$host = "http://localhost:3000"
 
 #api_keyの指定
-api_key = "PEsU4H8OhLgb5RontKp7xvJ6rSWVYTiflzAeMdXyDFBQj20Nk"
-api_key = "3WqjXzfcw8yUea02BNrKA154YvItsn6LipCJSukDTxoHM9hEF" #local
+api_key = "3WqjXzfcw8yUea02BNrKA154YvItsn6LipCJSukDTxoHM9hEF" #ダミー
 
 #registration_idの指定 EPNs.sendする場合は必須
-registration_id = "8edaIPAHJYcMpZqBm0nbkyTL5zWRXtvK9ghoOUVGEl1FiN7jS6wC3fu2QxsDr4"
-registration_id = "HkKTJrZCLlGFYMcAqtR7fg2u5dUnz1wBIO6vQN4xm9i0bey38PaDXESVsopjhW" #local
+registration_id = "HkKTJrZCLlGFYMcAqtR7fg2u5dUnz1wBIO6vQN4xm9i0bey38PaDXESVsopjhW" #ダミー
 
 #新しいregistration_idの取得
 #registration_id =  EPNs.register(api_key)
@@ -20,6 +18,7 @@ EPNs.connect(api_key, registration_id) do |object|
 end
 
 #クライアントにデータをpushする
+sleep(3) #connect直後だとsendしてもfailureになる
 p EPNs.send([registration_id], api_key, {:test=>123})
   
 #Threadで動いているので終わらない処理を書かないと終了する
